7dee3d760826c9ab97fa8dd7595ccde413e7c6d6
felipe-takaoka <fehtakaoka@gmail.com>
2021-09-06 21:14:06 -0300

    feat: Create transcript features

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ transcript_df = loadAndCleanTranscript()
 st.write(portfolio_df)
 st.write(profile_df)
 st.write(transcript_df.head(30))
+
+transcript_feats = createTranscriptFeatures(transcript_df, portfolio_df, profile_df)
+st.write(transcript_feats.head(50))
--- utils/extract_transform.py
+++ utils/extract_transform.py
@@ -91,3 +91,77 @@ def loadAndCleanTranscript():
   transcript_df["reward"] = transcript_df["reward"].fillna(0)
 
   return transcript_df
+
+
+@st.cache
+def createTranscriptFeatures(transcript_df, portfolio_df, profile_df):
+  """ xxx
+  """
+
+  transcript_feats = transcript_df.copy()
+
+  # Create dummy variables for the events (to perform a cumulative sum)
+  transcript_feats = pd.concat([transcript_feats, pd.get_dummies(transcript_feats["event"])], axis=1)
+
+  # Define the columns to aggregate, their function and their new name
+  agg_cols = {
+      # original column name: (agg function, agg column name)
+      "event_no": ("max", "event_no"),
+      "amount": ("sum", "spending"),
+      "reward": ("sum", "cum_reward"),
+      "transaction": ("sum", "transactions"),
+      "offer received": ("sum", "offers_received"),
+      "offer viewed": ("sum", "offers_viewed"),
+      "offer completed": ("sum", "offers_completed"),
+      "time": ("min", "min_time")
+  }
+  aggs = {k: v[0] for k, v in agg_cols.items()}
+  rename = {k:v[1] for k,v in agg_cols.items()}
+
+  # Perform the aggregations on the expanding window partitioned by each person
+  transcript_aggs = transcript_feats.groupby("person").expanding().agg(aggs)
+  transcript_aggs = transcript_aggs.rename(columns=rename)
+  transcript_aggs = transcript_aggs.reset_index().drop(columns="level_1")
+  transcript_feats = transcript_feats.merge(transcript_aggs, on=["person", "event_no"])
+
+  # Subtract the current "event" so that they account only for the past (without information not available on inference time)
+  cols_subtract = [col for col in agg_cols.keys() if col not in ["event_no", "time"]]
+  cols_keep = [rename[col] for col in cols_subtract]
+  transcript_feats.loc[:, cols_keep] -= transcript_feats.loc[:, cols_subtract].values
+
+  # Time since each person's first event
+  transcript_feats["time_customer"] = transcript_feats["time"] - transcript_feats["min_time"]
+  transcript_feats = transcript_feats.drop(columns="min_time")
+
+  # Average transaction value (up to that point)
+  transcript_feats["atv"] = transcript_feats["spending"] / transcript_feats["transactions"]
+  # Percentage of offers completed (completed / received) - up to that point
+  transcript_feats["offer_usage"] = transcript_feats["offers_completed"] / transcript_feats["offers_received"]
+
+  # Time since last transaction, offer received, offer viewed, and offer completed
+  events = ["offer received", "offer viewed", "transaction", "offer completed"]
+  transcript_feats = transcript_feats.drop(columns=events)
+  for event in events:
+      # Define auxiliary variables
+      event_count = rename[event]
+      event_name = event.replace(' ','_')
+      cols_aux = ["person", event_count, "time"]
+      col_last_event_at = f"last_{event_name}_at"
+      # Create filtered dataframe with the last event times
+      event_times = transcript_feats.loc[transcript_feats["event"]==event, cols_aux]
+      event_times[event_count] += 1
+      # Join the two dataframes and calculate the time since the last event
+      event_times = event_times.rename(columns={"time": col_last_event_at})
+      transcript_feats = transcript_feats.merge(event_times, on=["person", event_count], how="left")
+      transcript_feats[f"time_since_last_{event_name}"] = transcript_feats["time"] - transcript_feats[col_last_event_at]
+      transcript_feats = transcript_feats.drop(columns=col_last_event_at)
+
+  # Add offer data
+  portfolio_renamed = portfolio_df.copy()
+  portfolio_renamed.columns = [f"offer_{col}" if col!="offer_id" else col for col in portfolio_renamed]
+  transcript_feats = transcript_feats.merge(portfolio_renamed, on="offer_id", how="left")
+
+  # Add demografic data
+  transcript_feats = transcript_feats.merge(profile_df, on="person", how="left")
+
+  return transcript_feats

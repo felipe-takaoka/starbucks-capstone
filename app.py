7e2a1eea7c21e402b170e35553799e0d5617a773
felipe-takaoka <fehtakaoka@gmail.com>
2021-09-19 03:42:33 -0300

    feat: Display funnel chart

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import plotly.io as pio
 from utils.extract_transform import *
 
+# Set the default plotly theme
+pio.templates.default = "none"
+
+# Load and clean dataframes
 portfolio_df = cachedLoadAndCleanPortfolio()
 profile_df = cachedLoadAndCleanProfile()
 transcript_df = cachedLoadAndCleanTranscript()
 
-pages = ["Cleaned Inputs", "Customer Timeline", "Feature Engineering"]
+pages = ["Offers Portfolio", "Customer Timeline", "Feature Engineering"]
 page = st.sidebar.radio("Select page", pages)
 
-if page == "Cleaned Inputs":
-  st.write(portfolio_df)
-  st.write(profile_df.head(50))
-  st.write(transcript_df.head(50))
+st.title(page)
+
+if page == "Offers Portfolio":
+  promo_funnel = getPromoFunnel(transcript_df, portfolio_df)
+
+  st.subheader("Offer Funnel")
+  fig = go.Figure(data=[
+    go.Bar(name=step, x=promo_funnel[step], y=promo_funnel["code"], orientation="h")
+    for step in ["offer received", "offer viewed", "offer completed"]
+  ])
+  st.plotly_chart(fig)
+
+  st.subheader("Data")
+  st.write(promo_funnel)
+
 elif page == "Customer Timeline":
-  st.title("Customer Timeline")
+  pass
+
 elif page == "Feature Engineering":
   transcript_feats = createTranscriptFeatures(transcript_df, portfolio_df, profile_df)
   st.write(transcript_feats.head(50))
